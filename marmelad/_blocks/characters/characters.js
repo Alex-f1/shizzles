76dc741d4a0500f127a4b6303018b888ccb4aaff
Alex <skyriver.f1@gmail.com>
2022-07-07 16:12:10 +0500

    added table & fix

--- marmelad/_blocks/characters/characters.js
+++ marmelad/_blocks/characters/characters.js
@@ -1,6 +1,12 @@
-var swiper = new Swiper(".js-characters-slider", {
+const charactersSlider = new Swiper(".js-characters-slider", {
   slidesPerView: 'auto',
   spaceBetween: 20,
   loop: true,
   centeredSlides: true,
+  speed: 1000,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
 });
